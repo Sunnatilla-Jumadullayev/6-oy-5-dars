fe5694a8fc41854d9c2c1f0e0c95b3fd2d0e6c29
Sunnatilla-Jumadullayev <jumaduullayevsunnat@gmail.com>
2024-11-01 09:57:54 +0500

    first commit

---
+++ src/component/loading/Loading.jsx
@@ -0,0 +1,23 @@
+import "./loading.css";
+const Loading = () => {
+  return (
+    <div className="flex justify-center">
+      <div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
